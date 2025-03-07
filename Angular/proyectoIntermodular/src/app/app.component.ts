3808f5afd5e37eb5c965200bc3f67134301e40f1
Lxx367 <luciabraga2001@gmail.com>
2025-03-07 10:43:50 +0100

    commit arreglos en el diseño y funcionalidad

--- Angular/proyectoIntermodular/src/app/app.component.ts
+++ Angular/proyectoIntermodular/src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { MainPrincipalComponent } from "./components/main-principal/main-principal.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, HeaderComponent, RouterLink, RouterLinkActive, MainPrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'proyectoIntermodular';
+  isRouteActive = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isRouteActive = event.url !== '/';
+      }
+    });
+  }
+
+  onRouteActivate() {
+    this.isRouteActive = true;
+  }
 }
