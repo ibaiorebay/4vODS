271bf63c300d6325d4a379d25515b446edfcf02c
Youness <youmouhcine1@gmail.com>
2025-03-26 11:00:30 +0100

    Correcciones 1 Entrega, solucion de ids y mayor cantidad de datos

---
+++ Symfony/API/API/API/Models/iniciativa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Models;
+
+public partial class iniciativa
+{
+    public int ID_INICIATIVA { get; set; }
+
+    public string? TITULO { get; set; }
+
+    public int? HORAS { get; set; }
+
+    public string? FECHA_INICIO { get; set; }
+
+    public string? FECHA_FIN { get; set; }
+
+    public string? DESCRIPCION { get; set; }
+
+    public string? TIPO { get; set; }
+
+    public string? PRODUCTO_FINAL { get; set; }
+
+    public bool NUEVA { get; set; }
+
+    public string? DIFUSION { get; set; }
+
+    public virtual ICollection<asignatura> ID_ASIGNATURAs { get; set; } = new List<asignatura>();
+
+    public virtual ICollection<entidad> ID_ENTIDADs { get; set; } = new List<entidad>();
+
+    public virtual ICollection<meta> ID_METAs { get; set; } = new List<meta>();
+
+    public virtual ICollection<profesore> ID_PROFESORs { get; set; } = new List<profesore>();
+}
