b5b300f83693c5f05a8250ac8c6405aa526cb0a1
Asier <asielng2a@gmail.com>
2025-03-26 12:00:52 +0100

    Listas Auxiliares, y sus DTO

---
+++ Symfony/API/API/API/DTO/CursoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTO
+{
+    public class CursoDTO
+    {
+        public int ID_CURSO { get; set; }
+
+        public string NOMBRECURSO { get; set; }
+
+    }
+}
---
+++ Symfony/API/API/API/DTO/MetaDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTO
+{
+    public class MetaDTO
+    {
+        public int? ID_ODS { get; set; }
+        public int ID_META { get; set; }
+        public string? DESCRIPCION { get; set; }
+
+    }
+}
