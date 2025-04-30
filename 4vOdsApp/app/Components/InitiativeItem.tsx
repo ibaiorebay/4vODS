8d987000e3a858dd0258f4187d051322835cfc81
Ibai <ibai.loharces@gmail.com>
2025-04-30 12:30:17 +0200

    añadida barra de busqueda, pestañas y detalles pequeños además de las fotos de los ods

--- 4vOdsApp/app/Components/InitiativeItem.tsx
+++ 4vOdsApp/app/Components/InitiativeItem.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { Iniciativa } from '../Models/Iniciativa';
 import { Link } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+import { useMetas } from '../Hooks/UseMetas';
 
 interface InitiativeCardProps {
   iniciativa: Iniciativa;
 }
 
 const InitiativeCard: React.FC<InitiativeCardProps> = ({ iniciativa }) => {
+  const { findMetaById } = useMetas();
+
+  const metas = iniciativa.iD_METAs.map(id => findMetaById(iniciativa.iD_INICIATIVA));
+
+  let odsNames = '';
+  if (metas) {
+    odsNames = metas.map(meta => Array.isArray(meta?.iD_ODS) ? meta.iD_ODS.map(ods => ods.toString()).join(', ') : '').join(', ');
+  }
+
   return (
     <Link href={`/Views/Detail/${iniciativa.iD_INICIATIVA}`} style={styles.card}>
       <View style={styles.section}>
@@ -32,6 +42,14 @@ const InitiativeCard: React.FC<InitiativeCardProps> = ({ iniciativa }) => {
           <Text style={styles.dateText}>Fin: {iniciativa.fecha_FIN}</Text>
         </View>
       </View>
+
+      <View style={styles.odsContainer}>
+        <View style={styles.odsList}>
+          {metas.map(meta => (
+            <Image key={meta?.iD_META} source={{ uri: `assets/images/ods/${meta?.iD_ODS}.png` }} style={styles.odsImage} />
+          ))}
+        </View>
+      </View>
     </Link>
   );
 };
@@ -39,11 +57,10 @@ const InitiativeCard: React.FC<InitiativeCardProps> = ({ iniciativa }) => {
 const styles = StyleSheet.create({
   card: {
     backgroundColor: '#e3f2fd',
-    padding: 24,
+    padding: 12,
     borderRadius: 12,
     marginBottom: 20,
     marginHorizontal: 16,
-    marginTop: 16, 
     elevation: 5,
     shadowColor: '#1e88e5',
     shadowOffset: { width: 0, height: 4 },
@@ -76,6 +93,28 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#607d8b',
   },
+  odsContainer: {
+    backgroundColor: '#e3f2fd',
+    borderTopWidth: 1,
+    borderTopColor: '#90caf9',
+    paddingVertical: 8,
+  },
+  odsList: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  odsListItem: {
+    fontSize: 14,
+    color: '#607d8b',
+    marginRight: 8,
+  },
+  odsImage: {
+    width: 50,
+    height: 50,
+    margin: 4,
+  },
 });
 
 export default InitiativeCard;
+
--- 4vOdsApp/app/Views/InitiativeListScreen.tsx
+++ 4vOdsApp/app/Views/InitiativeListScreen.tsx
@@ -1,33 +1,83 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, Text, TextInput, View, StyleSheet, Button } from 'react-native';
 import { useInitiatives } from '../Hooks/UseInitiatives';
 import InitiativeCard from '../Components/InitiativeItem';
 import { Iniciativa } from '../Models/Iniciativa';
-import { router, useNavigation } from 'expo-router';
+import { useNavigation } from 'expo-router';
+import { useMetas } from '../Hooks/UseMetas';
 
-interface InitiativeListScreenProps {
-}
+interface InitiativeListScreenProps { }
 
 const InitiativeListScreen: React.FC<InitiativeListScreenProps> = () => {
   const { initiatives, error } = useInitiatives();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isInitiatives, setIsInitiatives] = useState(true);
   const navigation = useNavigation();
 
   useEffect(() => {
     navigation.setOptions({
-      title: 'Lista de Iniciativas', 
+      title: 'Lista de Iniciativas',
     });
   }, []);
 
+  const filteredInitiatives = initiatives.filter((iniciativa) =>
+    iniciativa.titulo.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <View>
-      <ScrollView>
-        {error && <Text style={{ color: 'red' }}>{error}</Text>}
-        {initiatives.map((iniciativa: Iniciativa) => (
-          <InitiativeCard key={iniciativa.iD_INICIATIVA} iniciativa={iniciativa} />
-        ))}
-      </ScrollView>
+      <View style={styles.buttonContainer}>
+            <View style={styles.buttons}>
+              <Button title="Iniciativas" onPress={() => setIsInitiatives(true)}/>
+            </View>
+            <View style={styles.buttons}>
+              <Button title="Indicadores" onPress={() => setIsInitiatives(false)}/>
+            </View>
+          </View>
+      {isInitiatives ? (
+        <ScrollView>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Buscar iniciativa"
+            onChangeText={(text) => setSearchTerm(text)}
+            value={searchTerm}
+          />
+          {error && <Text style={{ color: 'red' }}>{error}</Text>}
+          {filteredInitiatives.map((iniciativa: Iniciativa) => (
+            <InitiativeCard key={iniciativa.iD_INICIATIVA} iniciativa={iniciativa} />
+          ))}
+        </ScrollView>
+      ):
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>No hay indicadores disponibles</Text>
+      </View>
+      }
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  searchInput: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    marginVertical: 16,
+    margin: 16,
+  },
+  buttons: {
+    color: '#fff',
+    padding: 10,
+    borderRadius: 5,
+  },
+});
+
 export default InitiativeListScreen;
+
+
